8342209895fccee76bdff46b50ff46c71700b4ac
hesham <17p6020@eng.asu.edu.eg>
2021-04-24 16:44:17 +0200

    assets+data

---
+++ lib/Data.dart
@@ -0,0 +1,22 @@
+List<Map> paintings = [
+  {
+    'image': 'assets/painting1.jpeg',
+    'name': 'Sunflowers',
+  },
+  {
+    'image': 'assets/painting2.jpg',
+    'name': 'The Starry Night',
+  },
+  {
+    'image': 'assets/painting3.jpg',
+    'name': 'The Potato Eaters',
+  },
+  {
+    'image': 'assets/painting4.jpg',
+    'name': 'Irises',
+  },
+  {
+    'image': 'assets/painting5.jpg',
+    'name': 'Starry Night Over the Rhône',
+  },
+];
